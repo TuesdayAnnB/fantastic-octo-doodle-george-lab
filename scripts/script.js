9f496c9c3798614bee3233f90e6bd1fdcfc87211
TuesdayAnnB <40272506+TuesdayAnnB@users.noreply.github.com>
2024-11-05 22:47:59 +0000

    asadasdjgblbv

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ $("button").click(function(){
     $.getJSON("https://barrycumbie.github.io/barrycumbie/class-data.json", function(result){
       $.each(result, function(i, field){
         $("div").append(field + " ");
+        console.log(field + " ");
       });
     });
   });
