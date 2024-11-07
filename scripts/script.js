f94e4f9697e7ba5075d6fa7f73554f96fcbf278c
TuesdayAnnB <40272506+TuesdayAnnB@users.noreply.github.com>
2024-11-07 20:00:34 +0000

    aaaaaaa

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 
-    $.getJSON("https://barrycumbie.github.io/barrycumbie/class-data.json", function(result){
+    $.getJSON("https://tuesdayannb.github.io/TuesdayAnnB/class-data.json", function(result){
       $.each(result, function(i, field){
        $("div").append(field + " ");
-        console.log(field + " ");
       });
     });
