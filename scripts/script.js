bfa9123d273012d29f074fe53e8f1fd4b8d9a5c5
TuesdayAnnB <40272506+TuesdayAnnB@users.noreply.github.com>
2024-11-07 20:47:49 +0000

    eeeeee

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,17 @@
+//"https://tuesdayannb.github.io/TuesdayAnnB/class-data.json"
 
-    $.getJSON("https://tuesdayannb.github.io/TuesdayAnnB/class-data.json", function(result){
-      $.each(result, function(i, field){
-       $("div").append(field + " ");
-      });
-    });
+
+$.getJSON("https://tuesdayannb.github.io/TuesdayAnnB/class-data.json", function(data){
+  $("div").empty();
+  for (var i = 0; i < data.length; i++) {
+    objvalues = (Object.values(data[i]));
+    $.each(objvalues, function(key, value){
+      $("div").append(value + " ");
+    } );
+      $("div").append("<br>");
+      console.log("<br> loop " + i + "<br>");
+ }
+});
+//having trouble with the whole 'looping 3 times over' thing.
+
+
